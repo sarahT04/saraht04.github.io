1879d18d87797541e533a2a280b8638372b5b4d0
sarahT04 <sarahtanujaya@gmail.com>
2023-04-15 07:57:51 +0700

    FEAT: added illustration. project is now completed.

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -2,11 +2,12 @@
 	import HomeTagline from './home/HomeTagline.svelte';
 	import Navbar from './home/Navbar.svelte';
 	import Logos from './Logos.svelte';
-	import Illustration from './assets/img/Illustration.png';
+	import Me from './assets/svg/Me.svelte';
 </script>
 
-<main id="main" class="flex lg:p-20 xs:pt-0 justify-evenly lg:pb-2 p-10">
-	<img src={Illustration} alt="Illustration of Sarah" class="md:block hidden" />
+<main id="main" class="flex lg:p-20 xs:pt-0 justify-evenly lg:pb-2 p-10 overflow-y-visible">
+	<Me />
+	<!-- <img src={Illustration} alt="Illustration of Sarah" class="md:block hidden" /> -->
 	<div>
 		<Navbar />
 		<HomeTagline />
